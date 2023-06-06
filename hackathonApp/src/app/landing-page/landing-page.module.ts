1c2f540c0ec3e5bb7f37999a88b03f740905b078
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-06-06 18:30:04 -0500

    Feat: Added the resgister form component into the template

--- hackathonApp/src/app/landing-page/landing-page.module.ts
+++ hackathonApp/src/app/landing-page/landing-page.module.ts
@@ -6,13 +6,15 @@ import { HeaderComponent } from './shared/header/header.component';
 import { HackathonPageComponent } from './pages/hackathon-page/hackathon-page.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ResgisterFormComponent } from './components/resgister-form/resgister-form.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
     HackathonPageComponent,
-    FooterComponent
+    FooterComponent,
+    ResgisterFormComponent
   ],
   imports: [
     CommonModule,
