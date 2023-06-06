f06da12cf4cb45fac436d9e6e92bdd2115ace8cc
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-06-05 20:36:50 -0500

    Feat: Added the form validators assets

--- hackathonApp/src/app/landing-page/landing-page.module.ts
+++ hackathonApp/src/app/landing-page/landing-page.module.ts
@@ -3,18 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { HeaderComponent } from './shared/header/header.component';
+import { HackathonPageComponent } from './pages/hackathon-page/hackathon-page.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    HackathonPageComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
-    LandingPageRoutingModule
+    LandingPageRoutingModule,
+    ReactiveFormsModule
   ],
   exports:[
-    HeaderComponent
+    HackathonPageComponent
   ]
 })
 export class LandingPageModule { }
--- hackathonApp/src/app/landing-page/pages/hackathon-page/hackathon-page.component.ts
+++ hackathonApp/src/app/landing-page/pages/hackathon-page/hackathon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-hackathon-page',
@@ -7,11 +8,48 @@ import { Component } from '@angular/core';
 })
 export class HackathonPageComponent {
 
-  validPassword : any = '';
-  validEmail : any = '';
+  public email : string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
-  validData(data : any){
+  registerForm  : FormGroup = this.formBuilder.group({
+    fullName    : ['',[Validators.required]],
+    email       : ['',[Validators.required, Validators.pattern(this.email)]],
+    profile     : ['',[Validators.required]],
+    city        : ['',[Validators.required]],
+    country     : ['',[Validators.required]],
+  });
 
+  get emailTextValidator() : string{
+    const email = this.registerForm.get('email')?.errors;
+
+    if (email?.['required']){
+      return 'Este Campo No Debe Estar Vacío ';
+    }
+
+    if(email?.['pattern']){
+      return 'No parece un correo Valido';
+    }
+
+    return '';
   }
 
+  isValidData(formData : string): boolean{
+    return this.registerForm.controls[formData]?.invalid &&
+            this.registerForm.controls[formData]?.touched
+  }
+
+  submitFormMessage(): void{
+    if(this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+    }else{
+      window.location.href=`mailto:taxisLibresExample@hotmail.com?subjectRegistroDeUsuario&body=
+      %0ANombre%3A ${this.registerForm.controls['fullName'].value}
+      %0ACorreo%3A ${this.registerForm.controls['email'].value}
+      %0APerfil Ocupacional%3A ${this.registerForm.controls['profile'].value}
+      %0ACiudad%3A ${this.registerForm.controls['city'].value}
+      %0APaís%3A ${this.registerForm.controls['country'].value}`;
+      this.registerForm.reset('');
+    }
+  }
+
+  constructor(private formBuilder : FormBuilder,){}
 }
