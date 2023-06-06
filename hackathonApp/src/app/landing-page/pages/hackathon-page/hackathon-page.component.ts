26bbe68f32917d57cc036db7a67d21e82be96db5
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-06-05 19:34:01 -0500

    Added the main sections

---
+++ hackathonApp/src/app/landing-page/pages/hackathon-page/hackathon-page.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hackathon-page',
+  templateUrl: './hackathon-page.component.html',
+  styleUrls: ['./hackathon-page.component.scss']
+})
+export class HackathonPageComponent {
+
+  validPassword : any = '';
+  validEmail : any = '';
+
+  validData(data : any){
+
+  }
+
+}
