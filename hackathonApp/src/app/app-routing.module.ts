299c933793702797c6e86bc98bc172e9c9002200
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-06-06 19:48:41 -0500

    Feat: Added the routes aplication

--- hackathonApp/src/app/app-routing.module.ts
+++ hackathonApp/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'hackathon', loadChildren: ()=> import('./landing-page/landing-page.module').then(m => m.LandingPageModule)},
+  {path: '**', redirectTo: 'hackathon'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- hackathonApp/src/app/landing-page/landing-page-routing.module.ts
+++ hackathonApp/src/app/landing-page/landing-page-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HackathonPageComponent } from './pages/hackathon-page/hackathon-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    children:[
+      {
+        path:'home',
+        component:HackathonPageComponent
+      },
+      {
+        path:'**',
+        redirectTo:'home'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
